8ea4bdd6290181ecae2f4300d306bed875fdc63d
Arati Jagtap <154422939+ARATIJ7@users.noreply.github.com>
2024-07-06 19:38:46 +0530

    Create  security_groups.tf

---
+++ security_groups.tf
@@ -0,0 +1,38 @@
+# security_groups.tf
+resource "aws_security_group" "mongodb_sg1" {
+  provider = aws.region1
+  vpc_id = aws_vpc.vpc1.id
+
+  ingress {
+    from_port   = 27017
+    to_port     = 27017
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "mongodb_sg2" {
+  provider = aws.region2
+  vpc_id = aws_vpc.vpc2.id
+
+  ingress {
+    from_port   = 27017
+    to_port     = 27017
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
