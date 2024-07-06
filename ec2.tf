fc2f94c5ed83445a35a59d6f1a695a9c3461c07f
Arati Jagtap <154422939+ARATIJ7@users.noreply.github.com>
2024-07-06 20:01:23 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 # ec2.tf
 resource "aws_instance" "mongodb_instance1" {
   provider = aws.region1
-  ami = "ami-0c55b159cbfafe1f0"  # Replace with your AMI ID
+  ami = "ami-024ebc7de0fc64e44"  # Replace with your AMI ID
   instance_type = var.instance_type
   key_name = var.key_name
   security_groups = [aws_security_group.mongodb_sg1.name]
@@ -22,7 +22,7 @@ resource "aws_instance" "mongodb_instance1" {
 
 resource "aws_instance" "mongodb_instance2" {
   provider = aws.region2
-  ami = "ami-0c55b159cbfafe1f0"  # Replace with your AMI ID
+  ami = "ami-024ebc7de0fc64e44"  # Replace with your AMI ID
   instance_type = var.instance_type
   key_name = var.key_name
   security_groups = [aws_security_group.mongodb_sg2.name]
