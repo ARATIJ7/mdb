238396719a4f4321e8b6cef82935d43ef2fa5d27
Arati Jagtap <154422939+ARATIJ7@users.noreply.github.com>
2024-07-06 19:37:19 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -9,36 +9,18 @@ resource "aws_vpc" "vpc2" {
   cidr_block = "10.1.0.0/16"
 }
 
-resource "aws_security_group" "mongodb_sg1" {
+resource "aws_subnet" "subnet1" {
   provider = aws.region1
   vpc_id = aws_vpc.vpc1.id
-  ingress {
-    from_port   = 27017
-    to_port     = 27017
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone = "${var.aws_region_1}a"
 }
 
-resource "aws_security_group" "mongodb_sg2" {
+resource "aws_subnet" "subnet2" {
   provider = aws.region2
   vpc_id = aws_vpc.vpc2.id
-  ingress {
-    from_port   = 27017
-    to_port     = 27017
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  cidr_block = "10.1.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone = "${var.aws_region_2}a"
 }
