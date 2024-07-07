2bef7562ffee7a03448f9078497799ac10867d04
Arati <154422939+ARATIJ7@users.noreply.github.com>
2024-07-07 18:37:49 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,10 +1,9 @@
-# providers.tf
 provider "aws" {
   alias  = "region1"
-  region = var.aws_region_1
+  region = "us-west-2"
 }
 
 provider "aws" {
   alias  = "region2"
-  region = var.aws_region_2
+  region = "us-east-2"
 }
