2cc464f3113c964b8477dd76b12a0e4e4c821a8f
Arati <154422939+ARATIJ7@users.noreply.github.com>
2024-07-07 19:42:11 +0530

    Update subnets.tf

--- subnets.tf
+++ subnets.tf
@@ -3,6 +3,7 @@ resource "aws_subnet" "subnet1" {
   vpc_id             = aws_vpc.vpc1.id
   cidr_block         = "10.0.1.0/24"
   availability_zone  = "us-east-2a"
+  map_public_ip_on_launch = true
 
   tags = {
     Name = "subnet1"
@@ -14,6 +15,7 @@ resource "aws_subnet" "subnet2" {
   vpc_id             = aws_vpc.vpc2.id
   cidr_block         = "10.1.1.0/24"
   availability_zone  = "us-west-2a"
+  map_public_ip_on_launch = true
 
   tags = {
     Name = "subnet2"
