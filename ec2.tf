6e4fe4d5ad12c17d932d9a9428d2cc33ab7a1714
Arati <154422939+ARATIJ7@users.noreply.github.com>
2024-07-10 11:33:09 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,4 +1,3 @@
-# instances.tf
 resource "aws_instance" "mongodb_instance1" {
   provider                  = aws.region1
   ami                       = "ami-003932de22c285676"  # Replace with your AMI ID
@@ -16,17 +15,6 @@ resource "aws_instance" "mongodb_instance1" {
               sudo bash -c 'echo "replication:\n  replSetName: ${var.replica_set_name}" >> /etc/mongod.conf'
               sudo systemctl restart mongod
               sudo systemctl enable mongod
-
-              # Wait for MongoDB to start
-              sleep 30
-
-              # Initiate the replica set
-              mongo --eval 'rs.initiate({
-                _id: "${var.replica_set_name}",
-                members: [
-                  { _id: 0, host: "${aws_instance.mongodb_instance1.public_ip}:27017" }
-                ]
-              })'
               EOF
 
   tags = {
@@ -51,17 +39,6 @@ resource "aws_instance" "mongodb_instance2" {
               sudo bash -c 'echo "replication:\n  replSetName: ${var.replica_set_name}" >> /etc/mongod.conf'
               sudo systemctl restart mongod
               sudo systemctl enable mongod
-
-              # Wait for MongoDB to start
-              sleep 30
-
-              # Wait for the primary to be ready
-              until mongo --host ${aws_instance.mongodb_instance1.public_ip}:27017 --eval 'rs.status()'; do
-                sleep 5
-              done
-
-              # Add the secondary to the replica set
-              mongo --host ${aws_instance.mongodb_instance1.public_ip}:27017 --eval 'rs.add("${aws_instance.mongodb_instance2.public_ip}:27017")'
               EOF
 
   tags = {
