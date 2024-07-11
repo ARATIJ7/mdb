dbf5ca73345b710dae2e5ca2920c0f9f821a276d
Arati <154422939+ARATIJ7@users.noreply.github.com>
2024-07-11 16:46:40 +0530

    Update null resource.tf

--- null resource.tf
+++ null resource.tf
@@ -1,28 +1,34 @@
 resource "null_resource" "configure_mongodb_replica_set" {
-  provisioner "local-exec" {
-    command = <<-EOT
-              #!/bin/bash
-              PRIMARY_IP=${aws_instance.mongodb_instance1.public_ip}
-              SECONDARY_IP=${aws_instance.mongodb_instance2.public_ip}
+  provisioner "remote-exec" {
+    inline = [
+      "# Initiate the replica set on the primary instance",
+      "mongo --eval 'rs.initiate({_id: \"${var.replica_set_name}\", members: [{ _id: 0, host: \"${aws_instance.mongodb_instance1.public_ip}:27017\" }]})'",
 
-              # Initiate the replica set on the primary instance
-              ssh -o StrictHostKeyChecking=no -i ${var.key_name}.pem ubuntu@$PRIMARY_IP <<EOF
-              mongo --eval 'rs.initiate({
-                _id: "${var.replica_set_name}",
-                members: [
-                  { _id: 0, host: "$PRIMARY_IP:27017" }
-                ]
-              })'
-              EOF
+      "# Wait for the primary to be ready",
+      "sleep 60",
 
-              # Wait for the primary to be ready
-              sleep 60
+      "# Add the secondary to the replica set",
+      "mongo --eval 'rs.add(\"${aws_instance.mongodb_instance2.public_ip}:27017\")'"
+    ]
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = file("${var.key_name}.pem")
+      host        = aws_instance.mongodb_instance1.public_ip
+    }
+  }
 
-              # Add the secondary to the replica set
-              ssh -o StrictHostKeyChecking=no -i ${var.key_name}.pem ubuntu@$PRIMARY_IP <<EOF
-              mongo --eval 'rs.add("$SECONDARY_IP:27017")'
-              EOF
-              EOT
+  provisioner "remote-exec" {
+    inline = [
+      "# Add the secondary to the replica set",
+      "mongo --eval 'rs.add(\"${aws_instance.mongodb_instance2.public_ip}:27017\")'"
+    ]
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = file("${var.key_name}.pem")
+      host        = aws_instance.mongodb_instance1.public_ip
+    }
   }
 
   depends_on = [aws_instance.mongodb_instance1, aws_instance.mongodb_instance2]
